3211ef1a8e9a840ef12e761204e389f562f514c2
Jeremy Powell <jeremy@visionaid.com>
2024-01-09 09:36:57 +1300

    Add KeyUp test

--- WinUITestApp/MainWindow.xaml.cs
+++ WinUITestApp/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,11 +16,21 @@ namespace WinUITestApp
         public MainWindow()
         {
             this.InitializeComponent();
+
+            grid.KeyDown += Grid_KeyDown;
+
+            grid.KeyUp += Grid_KeyUp;
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void Grid_KeyDown(object sender, KeyRoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            Debug.WriteLine($"KeyDown: {e.Key}");
+        }
+
+        private void Grid_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            Debug.WriteLine($"KeyUp: {e.Key}");
         }
     }
+
 }
